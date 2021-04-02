aeff52ac7562d8909959220babfdac766ad2012e
noureldinelhelw <44075933+noureldinelhelw@users.noreply.github.com>
2021-04-02 17:40:37 -0400

    Added Challenge Description

--- directory-traversal-attack.py
+++ directory-traversal-attack.py
@@ -1,3 +1,13 @@
+#
+# There is a directory traversal vulnerability in the
+# following page http://127.0.0.1:8082/humantechconfig?file=human.conf
+# Write a script which will attempt various levels of directory
+# traversal to find the right amount that will give access
+# to the root directory. Inside will be a human.conf with the flag.
+#
+# Note: The script can timeout if this occurs try narrowing
+# down your search
+
 import urllib.request
 
 folder = ''
